b0ff34ee14103dc4c879a84bc12b8822d9af7854
ShvetGhare <ghareshvet@gmail.com>
2025-02-18 09:04:26 +0530

    4th commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // Use HashRouter here
 import "./index.css";
 import App from "./App.jsx";
 import ShopContextProvider from "./context/ShopContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <HashRouter>
+    {" "}
+    {/* Use HashRouter here instead of BrowserRouter */}
     <ShopContextProvider>
       <App />
     </ShopContextProvider>
